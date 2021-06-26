b8deb7ceffba78dac5f07a81d8bf315242e26a68
Bernice Opong Boateng <bboateng@jenkins.server>
2021-06-25 20:39:20 -0400

    all files

---
+++ elephant_castle/src/main/java/Elephant/Castle/elephant_castle/ElephantCastleApplication.java
@@ -0,0 +1,13 @@
+package Elephant.Castle.elephant_castle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantCastleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElephantCastleApplication.class, args);
+	}
+
+}
---
+++ elephant_castle/src/test/java/Elephant/Castle/elephant_castle/ElephantCastleApplicationTests.java
@@ -0,0 +1,13 @@
+package Elephant.Castle.elephant_castle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElephantCastleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
